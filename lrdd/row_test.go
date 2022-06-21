1ddf5f3d317fa11f27402c00dc15c5436848ffa1
Jaewan Park <jaewan@ab180.co>
2022-06-21 14:09:38 +0900

    feat: Add BenchmarkBytesCopy for check []byte copy peformance

--- lrdd/row_test.go
+++ lrdd/row_test.go
@@ -1,13 +1,70 @@
 package lrdd
 
 import (
+	"fmt"
 	"math"
+	"strings"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/protobuf/proto"
 )
+
+func BenchmarkBytesCopy(b *testing.B) {
+	benchs := []struct {
+		Name string
+		Func func(src, dst *[]byte, srcLen int)
+	}{
+		{
+			Name: "copy",
+			Func: func(src, dst *[]byte, srcLen int) {
+				copy(*dst, *src)
+			},
+		},
+		{
+			Name: "copy_partial",
+			Func: func(src, dst *[]byte, srcLen int) {
+				copy(*dst, (*src)[0:srcLen-1])
+			},
+		},
+		{
+			Name: "append",
+			Func: func(src, dst *[]byte, srcLen int) {
+				*dst = append((*dst)[:0], *src...)
+			},
+		},
+		{
+			Name: "append_partial",
+			Func: func(src, dst *[]byte, srcLen int) {
+				*dst = append((*dst)[:0], (*src)[0:srcLen-1]...)
+			},
+		},
+	}
+
+	srcLens := []int{1, 1024}
+
+	for _, bench := range benchs {
+		for _, srcLen := range srcLens {
+			b.Run(fmt.Sprintf("%v-%v", bench.Name, srcLen), func(b *testing.B) {
+				src := []byte(strings.Repeat("*", srcLen))
+
+				var dsts []*[]byte
+				for i := 0; i < b.N; i++ {
+					dst := make([]byte, 0, srcLen)
+					dsts = append(dsts, &dst)
+				}
+
+				b.ResetTimer()
+				for i := 0; i < b.N; i++ {
+					for _, dst := range dsts {
+						bench.Func(&src, dst, srcLen)
+					}
+				}
+			})
+		}
+	}
+}
 
 func BenchmarkRow_Marshal(b *testing.B) {
 	originRow := &Row{
