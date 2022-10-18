0674a62991d6eecb42f84085878db298ccddd719
Jaewan Park <jaewan@ab180.co>
2022-10-18 11:51:03 +0900

    test: Remove convey test for the visibility

--- test/context_cancel_test.go
+++ test/context_cancel_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/ab180/lrmr/test/integration"
 	. "github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/require"
 	"go.uber.org/goleak"
 )
 
@@ -38,22 +39,20 @@ func TestContextCancel(t *testing.T) {
 func TestContextCancel_WithinForLoop(t *testing.T) {
 	defer goleak.VerifyNone(t)
 
-	Convey("Running a job", t, integration.WithLocalCluster(2, func(cluster *integration.LocalCluster) {
+	integration.WithLocalCluster(2, func(cluster *integration.LocalCluster) {
 		ds := ContextCancelWithInputLoop()
 
-		Convey("It should be cancelled after cancelling the Context", func() {
-			ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-			defer cancel()
+		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+		defer cancel()
 
-			result, err := ds.RunAndCollect(ctx, cluster)
-			So(err, ShouldBeNil)
+		result, err := ds.RunAndCollect(ctx, cluster)
+		require.Nil(t, err)
 
-			err = result.Err()
-			if !errors.Is(err, context.DeadlineExceeded) {
-				t.Errorf("Expected DeadlineExceeded, got %v", err)
-			}
-		})
-	}))
+		err = result.Err()
+		if !errors.Is(err, context.DeadlineExceeded) {
+			t.Errorf("Expected DeadlineExceeded, got %v", err)
+		}
+	})()
 }
 
 func TestContextCancel_WithLocalPipes(t *testing.T) {
