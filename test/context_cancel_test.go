fa10d56f3a62ba88ff9d592cf9fc2eb2a4c88a81
Jaewan Park <jaewan@ab180.co>
2022-12-02 16:30:47 +0900

    chore: Remove convey package for the error visibility

--- test/context_cancel_test.go
+++ test/context_cancel_test.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/ab180/lrmr/test/integration"
-	. "github.com/smartystreets/goconvey/convey"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/goleak"
 )
@@ -15,25 +14,23 @@ import (
 func TestContextCancel(t *testing.T) {
 	defer goleak.VerifyNone(t)
 
-	Convey("Running a job", t, integration.WithLocalCluster(2, func(cluster *integration.LocalCluster) {
+	integration.WithLocalCluster(2, func(cluster *integration.LocalCluster) {
 		ds := ContextCancel(2 * time.Second)
 
-		Convey("It should be cancelled after cancelling the Context", func() {
-			ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-			defer cancel()
+		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+		defer cancel()
 
-			result, err := ds.RunAndCollect(ctx, cluster)
-			So(err, ShouldBeNil)
+		result, err := ds.RunAndCollect(ctx, cluster)
+		require.Nil(t, err)
 
-			err = result.Err()
-			if !errors.Is(err, context.DeadlineExceeded) {
-				t.Errorf("Expected DeadlineExceeded, got %v", err)
-			}
+		err = result.Err()
+		if !errors.Is(err, context.DeadlineExceeded) {
+			t.Errorf("Expected DeadlineExceeded, got %v", err)
+		}
 
-			time.Sleep(500 * time.Millisecond)
-			So(canceled.Load(), ShouldBeTrue)
-		})
-	}))
+		time.Sleep(500 * time.Millisecond)
+		require.True(t, canceled.Load())
+	})()
 }
 
 func TestContextCancel_WithinForLoop(t *testing.T) {
@@ -56,20 +53,18 @@ func TestContextCancel_WithinForLoop(t *testing.T) {
 func TestContextCancel_WithLocalPipes(t *testing.T) {
 	defer goleak.VerifyNone(t)
 
-	Convey("Running a job", t, integration.WithLocalCluster(2, func(cluster *integration.LocalCluster) {
+	integration.WithLocalCluster(2, func(cluster *integration.LocalCluster) {
 		ds := ContextCancelWithLocalPipe()
 
-		Convey("It should be cancelled after cancelling the Context", func() {
-			ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-			defer cancel()
+		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+		defer cancel()
 
-			result, err := ds.RunAndCollect(ctx, cluster)
-			So(err, ShouldBeNil)
+		result, err := ds.RunAndCollect(ctx, cluster)
+		require.Nil(t, err)
 
-			err = result.Err()
-			if !errors.Is(err, context.DeadlineExceeded) {
-				t.Errorf("Expected DeadlineExceeded, got %v", err)
-			}
-		})
-	}))
+		err = result.Err()
+		if !errors.Is(err, context.DeadlineExceeded) {
+			t.Errorf("Expected DeadlineExceeded, got %v", err)
+		}
+	})()
 }
