46632777616c533f168cb0c32f3ae1f5bd2d59c8
Jaewan Park <jaewan@ab180.co>
2022-12-05 14:04:18 +0900

    chore: Remove convey from complicated test for the error visibility

--- test/complicated_test.go
+++ test/complicated_test.go
@@ -2,50 +2,46 @@ package test
 
 import (
 	"testing"
+	"time"
 
-	"github.com/ab180/lrmr/metric"
+	lrmrmetric "github.com/ab180/lrmr/metric"
 	"github.com/ab180/lrmr/test/integration"
 	"github.com/ab180/lrmr/test/testdata"
-	"github.com/ab180/lrmr/test/testutils"
 	dto "github.com/prometheus/client_model/go"
-	. "github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/require"
 	"go.uber.org/goleak"
+	"golang.org/x/net/context"
 )
 
 func TestComplicatedQuery(t *testing.T) {
 	defer goleak.VerifyNone(t)
 
-	Convey("Given running nodes", t, integration.WithLocalCluster(4, func(cluster *integration.LocalCluster) {
-		Convey("When doing ComplicatedQuery", func() {
-			ds := ComplicatedQuery()
-			j, err := ds.RunInBackground(cluster)
-			So(err, ShouldBeNil)
+	integration.WithLocalCluster(4, func(cluster *integration.LocalCluster) {
+		ds := ComplicatedQuery()
+		j, err := ds.RunInBackground(cluster)
+		require.Nil(t, err)
 
-			Convey("It should be run without error", func() {
-				So(j.WaitWithContext(testutils.ContextWithTimeout()), ShouldBeNil)
+		ctx, _ := context.WithTimeout(context.Background(), 20*time.Second)
+		err = j.WaitWithContext(ctx)
+		require.Nil(t, err)
 
-				Convey("It should emit all metrics", func() {
-					m, err := j.Metrics()
-					So(err, ShouldBeNil)
+		m, err := j.Metrics()
+		require.Nil(t, err)
 
-					t.Logf("Metrics collected:\n%s", m.String())
-					So(m["Files"], ShouldEqual, testdata.TotalFiles)
-					So(m["Events"], ShouldEqual, testdata.TotalRows)
+		t.Logf("Metrics collected:\n%s", m.String())
 
-					// check prometheus metric; number of running tasks should be 0
-					metric := &dto.Metric{}
-					for _, w := range cluster.Executors {
-						err := lrmrmetric.RunningTasksGauge.
-							With(lrmrmetric.WorkerLabelValuesFrom(w.Node.Info())).
-							Write(metric)
+		require.Equal(t, testdata.TotalFiles, int(m["Files"]))
+		require.Equal(t, testdata.TotalRows, int(m["Events"]))
 
-						if err != nil {
-							So(err, ShouldBeNil)
-						}
-						So(metric.Gauge.GetValue(), ShouldEqual, 0)
-					}
-				})
-			})
-		})
-	}))
+		// check prometheus metric; number of running tasks should be 0
+		metric := &dto.Metric{}
+		for _, w := range cluster.Executors {
+			err := lrmrmetric.RunningTasksGauge.
+				With(lrmrmetric.WorkerLabelValuesFrom(w.Node.Info())).
+				Write(metric)
+			require.Nil(t, err)
+
+			require.Equal(t, float64(0), metric.Gauge.GetValue())
+		}
+	})()
 }
